a42bd6283fad3c0ce5c0944c88c333ab24cd277c
ChidinmaKO <kchidinmavictoria@gmail.com>
2019-08-08 22:48:32 +0200

    bite 161

---
+++ bites/bite161.py
@@ -0,0 +1,58 @@
+import os
+
+def count_dirs_and_files(directory='.'):
+    """Count the amount of of directories and files in passed in "directory" arg.
+       Return a tuple of (number_of_directories, number_of_files)
+    """
+    # number_of_directories = sum([len(dirnames) for _, dirnames, _ in os.walk(directory)])
+    # number_of_files = sum([len(filenames) for _, _, filenames in os.walk(directory)])
+    # return (number_of_directories, number_of_files)
+      
+    # OR  
+    number_of_directories = number_of_files = 0
+    for _, dirnames, filenames in os.walk(directory):
+        number_of_directories += len(dirnames)
+        number_of_files += len(filenames)
+    return (number_of_directories, number_of_files)
+
+# tests
+# import os
+from tempfile import TemporaryDirectory
+
+# from tree import count_dirs_and_files
+
+TMP = '/tmp'
+
+
+def test_only_files():
+    with TemporaryDirectory(dir=TMP) as dirname:
+        for i in range(5):
+            filename = f'{i}.txt'
+            path = os.path.join(dirname, filename)
+            with open(path, 'w') as f:
+                f.write('hello')
+
+        assert count_dirs_and_files(dirname) == (0, 5)
+
+
+def test_only_dirs():
+    with TemporaryDirectory(dir=TMP) as dirname:
+        for i in range(5):
+            os.makedirs(os.path.join(dirname, str(i)))
+
+        assert count_dirs_and_files(dirname) == (5, 0)
+
+
+def test_files_and_dirs():
+    with TemporaryDirectory(dir=TMP) as dirname:
+        for i in range(10):
+            if i % 2 == 0:
+                target_dir = os.path.join(dirname, str(i))
+                os.makedirs(target_dir)
+                for j in range(5):
+                    filename = f'{j}.txt'
+                    path = os.path.join(target_dir, filename)
+                    with open(path, 'w') as f:
+                        f.write('hello')
+
+        assert count_dirs_and_files(dirname) == (5, 25)
