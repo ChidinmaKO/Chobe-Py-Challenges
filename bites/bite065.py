b0cd557ace326ce53dd72b9113b4eb7d5b123a11
ChidinmaKO <kchidinmavictoria@gmail.com>
2019-11-10 23:29:50 +0100

    add bite 65

---
+++ bites/bite065.py
@@ -0,0 +1,56 @@
+import itertools
+import os
+import urllib.request
+
+# PREWORK
+DICTIONARY = os.path.join('/tmp', 'dictionary.txt')
+urllib.request.urlretrieve('http://bit.ly/2iQ3dlZ', DICTIONARY)
+
+with open(DICTIONARY) as f:
+    dictionary = set([word.strip().lower() for word in f.read().split()])
+
+
+def get_possible_dict_words(draw):
+    """Get all possible words from a draw (list of letters) which are
+       valid dictionary words. Use _get_permutations_draw and provided
+       dictionary"""
+    return set(word for word in _get_permutations_draw(draw) if word in dictionary)
+
+def _get_permutations_draw(draw):
+    """Helper to get all permutations of a draw (list of letters), hint:
+       use itertools.permutations (order of letters matters)"""
+    return {
+        "".join(x).lower()
+        for l in range(1, len(draw))
+        for x in itertools.permutations(draw, l)
+    }
+
+
+# tests
+
+# import itertools
+# import os
+# import urllib.request
+
+# PREWORK
+DICTIONARY = os.path.join('/tmp', 'dictionary.txt')
+urllib.request.urlretrieve('http://bit.ly/2iQ3dlZ', DICTIONARY)
+
+with open(DICTIONARY) as f:
+    dictionary = set([word.strip().lower() for word in f.read().split()])
+
+
+def get_possible_dict_words(draw):
+    """Get all possible words from a draw (list of letters) which are
+       valid dictionary words. Use _get_permutations_draw and provided
+       dictionary"""
+    permutations = [''.join(word).lower()
+                    for word in _get_permutations_draw(draw)]
+    return set(permutations) & set(dictionary)
+
+
+def _get_permutations_draw(draw):
+    """Helper to get all permutations of a draw (list of letters), hint:
+       use itertools.permutations (order of letters matters)"""
+    for i in range(1, 8):
+        yield from list(itertools.permutations(draw, i))
